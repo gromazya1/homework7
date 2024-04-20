e404b7f6a3e23f819bc31d4b32c223c32a213d45
Kirill Gromazin <167010706+gromazya1@users.noreply.github.com>
2024-04-20 14:53:17 +0500

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,36 @@
+x = 50
+print('privet')
+if x <0:
+    print('меньше нуля')
+print('poka')
+
+a,b = 228, 1337
+if a < b:
+    print('a < b')
+
+if a < b and a > 0:
+    print('ok')
+
+if (a < b) and (a > 0 or b > 999):
+    print('ok X2')
+
+if 1336 < b and b < 1338:
+    print('norm')
+
+if '16' > '150':
+    print('good')
+
+if '166' > '16':
+    print('horosho')
+
+if [7, 2] > [1, 7]:
+    print('ok')
+
+ #if '4895489648' > 1:
+     #print('somnitelno')
+
+ #if [5, 6] > 12:
+     #print('nea')
+
+#if '100000' != 100000:
+     #print('bad')
